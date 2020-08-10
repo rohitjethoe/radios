4baa4b0d339a1a955227602ed2646c015970280f
Rohit <61277504+rohitchai@users.noreply.github.com>
2020-08-10 14:45:15 +0200

    Added header component

---
+++ client/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="header">
+        <div class="logo">
+            Radio Finder
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .logo {
+        font-size: 24px;
+        padding: 5px;
+    }
+</style>
