20412d40ddb569c270bcf65a750524586dd8a0ec
Rohit <61277504+rohitchai@users.noreply.github.com>
2020-08-10 20:43:19 +0200

    Added find by continent name to REST API functions.

--- server/api/index.js
+++ server/api/index.js
@@ -8,13 +8,21 @@ router.get('/radios', (req, res) => {
     res.send(data)
 })
 
-router.get('/radios/:country', (req, res) => {
+router.get('/radios/:continent/', (req, res) => {
     const data = require('../json/radios.json');
+    let continent = req.params.continent;
+
+    res.send(data['radios'][continent]);
+})
+
+router.get('/radios/:continent/:country', (req, res) => {
+    const data = require('../json/radios.json');
+    let continent = req.params.continent;
     let country = req.params.country;
 
     country = country.toUpperCase();
 
-    res.send(data['radios'][country]);
+    res.send(data['radios'][continent][country]);
 })
 
 module.exports = router;
