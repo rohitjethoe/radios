3d029334df734c3e17f45f531936ce09e153603a
Rohit <61277504+rohitchai@users.noreply.github.com>
2020-08-10 14:45:44 +0200

    Added header component to App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="app">
+    <Header />
+    <Map />
   </div>
 </template>
 
 <script>
+import Header from './components/Header';
+import Map from './components/Map';
+
 export default {
   name: 'App',
   components: {
+    Header,
+    Map
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
