f89a89e52e9f0fe636d4dd92a890616bf2a78533
Rohit <61277504+rohitchai@users.noreply.github.com>
2020-08-10 23:56:51 +0200

    Fixed audio to bottom.

--- client/src/components/Radio.vue
+++ client/src/components/Radio.vue
@@ -1,18 +1,20 @@
 <template>
     <div>
-        <audio autoplay controls :src="href">
-            
-        </audio>
+        <audio autoplay controls :src="href"></audio>
     </div>
 </template>
 
 <script>
 export default {
     name: "Radio",
-    props: ["href"]
+    props: ["href"],
 }
 </script>
 
 <style scoped>
-
+    audio {
+        width: 100%;
+        position: fixed;
+        bottom: 0;
+    }
 </style>
