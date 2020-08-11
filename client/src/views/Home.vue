e8aefa232de633bcbd33f79bbf646b9d2d1bdf5f
Rohit <61277504+rohitchai@users.noreply.github.com>
2020-08-11 09:57:42 +0200

    Added home view.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,78 @@
 <template>
-  <div class="home">
+  <div id="Home">
+    <Header v-bind:map="map" v-on:map-status="changeMapStatus()"/>
+    <div class="choose-station">
+      <h1><span>📻</span> Choose a radio station.</h1>
+    </div>
+    <Map v-if="map" />
+    <List v-bind:data="data" v-on:change-radio="changeRadio" v-if="!map"/>
+    <Radio v-bind:href="href" />
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+import Header from '../components/Header';
+import Map from '../components/Map';
+import List from '../components/List';
+import Radio from '../components/Radio';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      map: false,
+      data: null,
+      href: null,
+      station: undefined,
+      country: undefined,
+      continent: undefined
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:5000/api/radios')
+    .then((res) => {
+      this.data = res.data;
+    })
+  },
+  methods: {
+    changeMapStatus() {
+      this.map = !this.map;
+    },
+
+    changeRadio(href) {
+      this.href = href;
+    }
+  },
   components: {
-    
+    Header,
+    Map,
+    List,
+    Radio
   }
 }
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.choose-station {
+  margin-top: 5%;
+  margin-left: 5%;
+}
+
+.choose-station span {
+  margin-right: 10px;
+}
+</style>
