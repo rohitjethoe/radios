f3b1c00cb1c3ee979f2d4d597e47f832305d0c87
Rohit <61277504+rohitchai@users.noreply.github.com>
2020-08-11 09:53:43 +0200

    Added Vue Router.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,77 +1,32 @@
 <template>
   <div id="app">
-    <Header v-bind:map="map" v-on:map-status="changeMapStatus()"/>
-    <div class="choose-station">
-      <h1><span>📻</span> Choose a radio station.</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <Map v-if="map" />
-    <List v-bind:data="data" v-on:change-radio="changeRadio" v-if="!map"/>
-    <Radio v-bind:href="href" />
+    <router-view/>
   </div>
 </template>
 
-<script>
-import axios from 'axios';
-
-import Header from './components/Header';
-import Map from './components/Map';
-import List from './components/List';
-import Radio from './components/Radio';
-
-export default {
-  name: 'App',
-  data() {
-    return {
-      map: false,
-      data: null,
-      href: null,
-      station: undefined,
-      country: undefined,
-      continent: undefined
-    }
-  },
-  mounted() {
-    axios.get('http://localhost:5000/api/radios')
-    .then((res) => {
-      this.data = res.data;
-    })
-  },
-  methods: {
-    changeMapStatus() {
-      this.map = !this.map;
-    },
-    changeRadio(href) {
-      this.href = href;
-    }
-  },
-  components: {
-    Header,
-    Map,
-    List,
-    Radio
-  }
-}
-</script>
-
 <style>
-* {
-  margin: 0;
-  padding: 0;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
 }
 
-.choose-station {
-  margin-top: 5%;
-  margin-left: 5%;
+#nav {
+  padding: 30px;
 }
 
-.choose-station span {
-  margin-right: 10px;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
